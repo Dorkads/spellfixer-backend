ed55bc491889a7dc1d948e67389f5d0c198c019c
Ruslan <homastavi@gmail.com>
2025-06-23 00:30:34 +0300

    Добавление артефактов ML, изменения для скрипта под прод

--- ml_model.py
+++ ml_model.py
@@ -3,11 +3,12 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
 
-BASE = os.path.dirname(__file__)
-MODEL_PATH = os.path.join(BASE, "../spellfixer-ml/model/model.keras")
-TOK_PATH   = os.path.join(BASE, "../spellfixer-ml/model/tokenizer/tokenizer.json")
-CORR_PATH  = os.path.join(BASE, "corrections.json")
-MAX_LEN    = 25
+BASE   = os.path.dirname(__file__)
+
+MODEL_PATH = os.path.join(BASE, "ml_artifacts", "model.keras")
+TOK_PATH   = os.path.join(BASE, "ml_artifacts", "tokenizer.json")
+CORR_PATH  = os.path.join(BASE, "ml_artifacts", "corrections.json")
+MAX_LEN    = int(os.getenv("MAX_LEN", 25))
 
 # загрузка один раз при импорте
 with open(TOK_PATH, 'r', encoding='utf-8') as f:
